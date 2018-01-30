e3966dcc3972403390ef7daabec55dff980f9422
Guy Lichtman <1395797+glicht@users.noreply.github.com>
2018-01-30 21:45:31 +0200

    Update whitelist file with support for S3 (#9)
    * update whitelist file with support for S3
    
    * unit test to check s3 parameter whitelist

--- aws-xray-recorder-sdk-aws-sdk/src/test/java/com/amazonaws/xray/handlers/TracingHandlerTest.java
+++ aws-xray-recorder-sdk-aws-sdk/src/test/java/com/amazonaws/xray/handlers/TracingHandlerTest.java
@@ -2,12 +2,14 @@ package com.amazonaws.xray.handlers;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.entity.BasicHttpEntity;
+import org.apache.http.impl.io.EmptyInputStream;
 import org.apache.http.message.BasicHttpResponse;
 import org.apache.http.message.BasicStatusLine;
 import org.apache.http.protocol.HttpContext;
@@ -29,6 +31,8 @@ import com.amazonaws.services.lambda.AWSLambda;
 import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
 import com.amazonaws.services.lambda.model.InvokeRequest;
 import com.amazonaws.services.lambda.model.InvokeResult;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.emitters.Emitter;
@@ -45,26 +49,35 @@ public class TracingHandlerTest {
         AWSXRay.setGlobalRecorder(AWSXRayRecorderBuilder.standard().withEmitter(blankEmitter).build());
         AWSXRay.clearTraceEntity();
     }
+    
+    private void mockHttpClient(Object client, String responseContent) {
+        AmazonHttpClient amazonHttpClient = new AmazonHttpClient(new ClientConfiguration());
+        ConnectionManagerAwareHttpClient apacheHttpClient = Mockito.mock(ConnectionManagerAwareHttpClient.class);
+        HttpResponse httpResponse = new BasicHttpResponse(new BasicStatusLine(HttpVersion.HTTP_1_1, 200, "OK"));
+        BasicHttpEntity responseBody = new BasicHttpEntity();
+        InputStream in = EmptyInputStream.INSTANCE;
+        if(null != responseContent && !responseContent.isEmpty()) {
+            in = new ByteArrayInputStream(responseContent.getBytes(StandardCharsets.UTF_8));
+        }
+        responseBody.setContent(in); 
+        httpResponse.setEntity(responseBody);
+
+        try {
+            Mockito.doReturn(httpResponse).when(apacheHttpClient).execute(Mockito.any(HttpUriRequest.class), Mockito.any(HttpContext.class));
+        } catch (IOException e) { 
+            System.err.println("Exception during mock: " + e);
+        }
+
+        Whitebox.setInternalState(amazonHttpClient, "httpClient", apacheHttpClient);
+        Whitebox.setInternalState(client, "client", amazonHttpClient);
+    }
 
     @Test
     public void testLambdaInvokeSubsegmentContainsFunctionName() {
         // Setup test
         AWSLambda lambda = AWSLambdaClientBuilder.standard().withRequestHandlers(new TracingHandler()).withRegion(Regions.US_EAST_1).withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("fake", "fake"))).build();
-
-        AmazonHttpClient amazonHttpClient = new AmazonHttpClient(new ClientConfiguration());
-        ConnectionManagerAwareHttpClient apacheHttpClient = Mockito.mock(ConnectionManagerAwareHttpClient.class);
-        HttpResponse httpResponse = new BasicHttpResponse(new BasicStatusLine(HttpVersion.HTTP_1_1, 200, "OK"));
-        BasicHttpEntity responseBody = new BasicHttpEntity();
-        responseBody.setContent(new ByteArrayInputStream("null".getBytes(StandardCharsets.UTF_8))); // Lambda returns "null" on successful fn. with no return value
-        httpResponse.setEntity(responseBody);
-
-        try {
-            Mockito.doReturn(httpResponse).when(apacheHttpClient).execute(Mockito.any(HttpUriRequest.class), Mockito.any(HttpContext.class));
-        } catch (IOException e) { }
-
-        Whitebox.setInternalState(amazonHttpClient, "httpClient", apacheHttpClient);
-        Whitebox.setInternalState(lambda, "client", amazonHttpClient);
-
+        mockHttpClient(lambda, "null"); // Lambda returns "null" on successful fn. with no return value
+        
         // Test logic
         Segment segment = AWSXRay.beginSegment("test");
 
@@ -78,6 +91,46 @@ public class TracingHandlerTest {
         Assert.assertEquals("Invoke", segment.getSubsegments().get(0).getAws().get("operation"));
         System.out.println(segment.getSubsegments().get(0).getAws());
         Assert.assertEquals("testFunctionName", segment.getSubsegments().get(0).getAws().get("function_name"));
+    }
+    
+    @Test
+    public void testS3PutObjectSubsegmentContainsBucketName() {
+        // Setup test
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRequestHandlers(new TracingHandler()).withRegion(Regions.US_EAST_1).withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("fake", "fake"))).build();        
+        mockHttpClient(s3, null);
+        
+        final String BUCKET = "test-bucket", KEY = "test-key";
+        // Test logic 
+        Segment segment = AWSXRay.beginSegment("test");        
+        s3.putObject(BUCKET, KEY, "This is a test from java");               
+        Assert.assertEquals(1, segment.getSubsegments().size());
+        Assert.assertEquals("PutObject", segment.getSubsegments().get(0).getAws().get("operation"));
+        System.out.println(segment.getSubsegments().get(0).getAws());
+        Assert.assertEquals(BUCKET, segment.getSubsegments().get(0).getAws().get("bucket_name"));
+        Assert.assertEquals(KEY, segment.getSubsegments().get(0).getAws().get("key"));
+    }
+    
+    @Test
+    public void testS3CopyObjectSubsegmentContainsBucketName() {
+        // Setup test
+        final String copyResponse = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + 
+                "<CopyObjectResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">" + 
+                "<LastModified>2018-01-21T10:09:54.000Z</LastModified><ETag>&quot;31748afd7b576119d3c2471f39fc7a55&quot;</ETag>" + 
+                "</CopyObjectResult>";
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRequestHandlers(new TracingHandler()).withRegion(Regions.US_EAST_1).withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("fake", "fake"))).build();        
+        mockHttpClient(s3, copyResponse);
+        
+        final String BUCKET = "source-bucket", KEY = "source-key", DST_BUCKET = "dest-bucket", DST_KEY = "dest-key";
+        // Test logic 
+        Segment segment = AWSXRay.beginSegment("test");        
+        s3.copyObject(BUCKET, KEY, DST_BUCKET, DST_KEY);               
+        Assert.assertEquals(1, segment.getSubsegments().size());
+        Assert.assertEquals("CopyObject", segment.getSubsegments().get(0).getAws().get("operation"));
+        System.out.println(segment.getSubsegments().get(0).getAws());
+        Assert.assertEquals(BUCKET, segment.getSubsegments().get(0).getAws().get("source_bucket_name"));
+        Assert.assertEquals(KEY, segment.getSubsegments().get(0).getAws().get("source_key"));
+        Assert.assertEquals(DST_BUCKET, segment.getSubsegments().get(0).getAws().get("destination_bucket_name"));
+        Assert.assertEquals(DST_KEY, segment.getSubsegments().get(0).getAws().get("destination_key"));
     }
 
 }
