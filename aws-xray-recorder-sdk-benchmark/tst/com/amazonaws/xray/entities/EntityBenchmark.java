4471ddb5bfa9b10cd6c7cc3afd1b17b39dc4f51e
Jeffery Saeteurn <cssaete@amazon.com>
2019-06-12 12:24:59 -0700

    Initial commit of benchmark.

---
+++ aws-xray-recorder-sdk-benchmark/tst/com/amazonaws/xray/entities/EntityBenchmark.java
@@ -0,0 +1,111 @@
+package com.amazonaws.xray.entities;
+
+import com.amazonaws.xray.AWSXRayRecorder;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.net.SocketException;
+import java.util.concurrent.TimeUnit;
+
+public class EntityBenchmark {
+    public static final String SEGMENT_NAME = "BENCHMARK_SEGMENT";
+
+    // Benchmark state that initializes a parent segment to operate on.
+    @State(Scope.Thread)
+    public static class BenchmarkState {
+
+        // Recorder which always chooses to sample.
+        public Segment parentSegment;
+
+        // X-Ray Recorder
+        public AWSXRayRecorder recorder;
+
+        // An exception object
+        public Exception theException;
+
+        @Setup(Level.Trial)
+        public void setupOnce() throws SocketException {
+            recorder = AWSXRayRecorderBuilder.defaultRecorder();
+            theException = new Exception("Test Exception");
+        }
+
+        @Setup(Level.Invocation)
+        public void setup() {
+            parentSegment = new SegmentImpl(recorder, SEGMENT_NAME);
+        }
+
+        @TearDown(Level.Invocation)
+        public void doTearDown() {
+            for (Subsegment subsegment : parentSegment.getSubsegments()) {
+                parentSegment.removeSubsegment(subsegment);
+            }
+        }
+    }
+
+    // Construct a segment
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value=1)
+    @Warmup(iterations = 20)
+    @Measurement(iterations = 20)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public Segment constructSegmentBenchmark(BenchmarkState state) {
+        return new SegmentImpl(state.recorder, SEGMENT_NAME);
+    }
+
+    // Construct a subsegment and add it to the parent segment.
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value=1)
+    @Warmup(iterations = 20)
+    @Measurement(iterations = 20)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public Subsegment constructSubsegmentPutInSegmentBenchmark(BenchmarkState state) {
+        // TODO: Find a way to create just the subsegment and not force it into the parent segment?
+        return new SubsegmentImpl(state.recorder, SEGMENT_NAME, state.parentSegment);
+    }
+
+    // Add an annotation to a segment
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value=1)
+    @Warmup(iterations = 20)
+    @Measurement(iterations = 20)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void putAnnotationBenchmark(BenchmarkState state) {
+        state.parentSegment.putAnnotation("Key", "Value");
+    }
+
+    // Add metadata to a segment
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value=1)
+    @Warmup(iterations = 20)
+    @Measurement(iterations = 20)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void putMetadataBenchmark(BenchmarkState state) {
+        state.parentSegment.putMetadata("Key", "Value");
+    }
+
+    // Add exception into a segment
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value=1)
+    @Warmup(iterations = 20)
+    @Measurement(iterations = 20)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public void putExceptionSegmentBenchmark(BenchmarkState state) {
+        state.parentSegment.addException(state.theException);
+    }
+}
---
+++ aws-xray-recorder-sdk-benchmark/tst/com/amazonaws/xray/strategy/sampling/LocalizedSamplingStrategyBenchmark.java
@@ -0,0 +1,68 @@
+package com.amazonaws.xray.strategy.sampling;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class LocalizedSamplingStrategyBenchmark {
+
+    @State(Scope.Thread)
+    public static class DefaultSamplingRulesState {
+        private LocalizedSamplingStrategy samplingStrategy;
+        private SamplingRequest samplingRequest;
+
+        @Setup(Level.Trial)
+        public void setupOnce() {
+            samplingStrategy = new LocalizedSamplingStrategy();
+            samplingRequest = new SamplingRequest("roleARN", "arn:aws:execute-api:us-east-1:1234566789012:qsxrty/test",
+                    "serviceName", "hostName", "someMethod", "https://hostName.com", null, null);
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class NoSampleSamplingRulesState {
+        private LocalizedSamplingStrategy samplingStrategy;
+        private SamplingRequest samplingRequest;
+        private static final URL NO_SAMPLE_RULE = LocalizedSamplingStrategy.class.getResource("/com/amazonaws/xray/strategy/sampling/NoSampleRule.json");
+
+        @Setup(Level.Trial)
+        public void setupOnce() {
+            samplingStrategy = new LocalizedSamplingStrategy(NO_SAMPLE_RULE);
+            samplingRequest = new SamplingRequest("roleARN", "arn:aws:execute-api:us-east-1:1234566789012:qsxrty/test",
+                    "serviceName", "hostName", "someMethod", "https://hostName.com", null, null);
+        }
+    }
+
+    // Benchmark default sampling rules on a sampling Request that matches the rules.
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value=1)
+    @Warmup(iterations = 20)
+    @Measurement(iterations = 20)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public boolean defaultSamplingRuleBenchmark(DefaultSamplingRulesState state) {
+        return state.samplingStrategy.shouldTrace(state.samplingRequest).isSampled();
+    }
+
+    // Benchmark a no match sampling rule on a sampling request.
+    @Benchmark
+    @BenchmarkMode(Mode.All)
+    @Fork(value=1)
+    @Warmup(iterations = 20)
+    @Measurement(iterations = 20)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    public boolean noSampleSamplingBenchmark(NoSampleSamplingRulesState state) {
+        return state.samplingStrategy.shouldTrace(state.samplingRequest).isSampled();
+    }
+}
