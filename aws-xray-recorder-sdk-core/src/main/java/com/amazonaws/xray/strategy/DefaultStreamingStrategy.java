bd017b0e9568eed65154ba7a1e9753aed111f8eb
Kaushal Partani <partani@amazon.com>
2019-08-13 14:05:05 -0700

    Updated Default Streaming Strategy and added Unit Tests

--- aws-xray-recorder-sdk-core/src/main/java/com/amazonaws/xray/strategy/DefaultStreamingStrategy.java
+++ aws-xray-recorder-sdk-core/src/main/java/com/amazonaws/xray/strategy/DefaultStreamingStrategy.java
@@ -11,9 +11,7 @@ import com.amazonaws.xray.entities.Segment;
 import com.amazonaws.xray.entities.Subsegment;
 
 public class DefaultStreamingStrategy implements StreamingStrategy {
-    private static final Log logger =
-        LogFactory.getLog(DefaultStreamingStrategy.class);
-
+    private static final Log logger = LogFactory.getLog(DefaultStreamingStrategy.class);
 
     private static final int DEFAULT_MAX_SEGMENT_SIZE = 100;
 
@@ -25,9 +23,7 @@ public class DefaultStreamingStrategy implements StreamingStrategy {
      * Constructs an instance of DefaultStreamingStrategy using the default {@code maxSegmentSize} of 100.
      *
      */
-    public DefaultStreamingStrategy() {
-        this(DEFAULT_MAX_SEGMENT_SIZE);
-    }
+    public DefaultStreamingStrategy() { this(DEFAULT_MAX_SEGMENT_SIZE); }
 
     /**
      * {@inheritDoc}
@@ -65,26 +61,52 @@ public class DefaultStreamingStrategy implements StreamingStrategy {
     /**
      * {@inheritDoc}
      *
-     * Performs a depth first search to find completed subsegment subtrees. Serializes these subsegments, streams them to the daemon, and removes them from their parents.
+     * Performs Subtree Subsegment Streaming to stream completed subsegment subtrees. Serializes these subtrees of subsegments, streams them to the daemon, and removes them from their parents.
      *
      * @see StreamingStrategy#streamSome(Entity,Emitter)
      */
+
     public void streamSome(Entity entity, Emitter emitter) {
         if (entity.getSubsegmentsLock().tryLock()) {
             try {
-                //copy before traversal to avoid ConcurrentModificationExceptions
-                new ArrayList<Subsegment>(entity.getSubsegments()).forEach( (subsegment) -> {
-                    if (subsegment.isInProgress() || !subsegment.getSubsegments().isEmpty()) {
-                        streamSome(subsegment, emitter);
-                    } else {
-                        emitter.sendSubsegment(subsegment);
-                        subsegment.setEmitted(true);
-                        entity.removeSubsegment(subsegment);
-                    }
-                });
+                stream(entity, emitter);
             } finally {
                 entity.getSubsegmentsLock().unlock();
             }
         }
+    }
+
+    private boolean stream(Entity entity, Emitter emitter) {
+        ArrayList<Subsegment> children = new ArrayList<>(entity.getSubsegments());
+        ArrayList<Subsegment> streamable = new ArrayList<>();
+
+        //Gather children and in the condition they are ready to stream, add them to the streamable list.
+        if (children.size() > 0) {
+            for (Subsegment child : children) {
+                if (child.getSubsegmentsLock().tryLock()) {
+                    try {
+                        if (stream(child, emitter)) {
+                            streamable.add(child);
+                        }
+                    } finally {
+                        child.getSubsegmentsLock().unlock();
+                    }
+                }
+            }
+        }
+
+        //A subsegment is marked streamable if all of its children are streamable and the entity itself is not in progress.
+        if (children.size() == streamable.size() && !entity.isInProgress()) {
+            return true;
+        }
+
+        //Stream the subtrees that are ready.
+        for (Subsegment child : streamable) {
+            emitter.sendSubsegment(child);
+            child.setEmitted(true);
+            entity.removeSubsegment(child);
+        }
+
+        return false;
     }
 }
