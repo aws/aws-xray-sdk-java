ec0c8ea869df3a5eb7716210a61ee6174b128b87
Shengxin Li <shengxil@amazon.com>
2019-11-05 16:15:17 -0800

    Adding an generic SQL recorder

---
+++ aws-xray-recorder-sdk-sql/src/main/java/com/amazonaws/xray/sql/TracingDataSource.java
@@ -0,0 +1,82 @@
+package com.amazonaws.xray.sql;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+public class TracingDataSource implements DataSource {
+
+    protected DataSource delegate;
+
+    public TracingDataSource(DataSource dataSource) {
+        this.delegate = dataSource;
+    }
+
+    /**
+     * Call {@code dataSource = TracingDataSource.decorate(dataSource)} to decorate your {@link DataSource} before any calls
+     * to #getConnection in order to have all your SQL queries recorded with an X-Ray Subsegment.
+     *
+     * @param dataSource the datasource to decorate
+     * @return a DataSource that traces all SQL queries in X-Ray
+     */
+    public static DataSource decorate(DataSource dataSource) {
+        return new TracingDataSource(dataSource);
+    }
+
+    /**
+     *  Traced methods
+     */
+
+    @Override
+    public Connection getConnection() throws SQLException {
+        return new TracingConnection(delegate.getConnection());
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return new TracingConnection(delegate.getConnection(username, password));
+    }
+
+    /**
+     * Plain methods
+     */
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return delegate.unwrap(iface);
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return delegate.isWrapperFor(iface);
+    }
+
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        return delegate.getLogWriter();
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        delegate.setLogWriter(out);
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        delegate.setLoginTimeout(seconds);
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        return delegate.getLoginTimeout();
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return delegate.getParentLogger();
+    }
+}
