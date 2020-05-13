ef470b39e56ce5740f7a75c398c425b05c815bdd
William Armiros <armiros@amazon.com>
2020-05-12 23:33:15 -0500

    Removed unused imports

--- aws-xray-recorder-sdk-sql/src/test/java/com/amazonaws/xray/sql/TracingDataSourceTest.java
+++ aws-xray-recorder-sdk-sql/src/test/java/com/amazonaws/xray/sql/TracingDataSourceTest.java
@@ -7,9 +7,6 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import java.sql.Connection;
 import java.sql.SQLException;
 
 import javax.sql.DataSource;
