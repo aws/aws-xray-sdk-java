04748466d00160ac82a8b9165cb657e13184fa06
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-05-20 13:07:09 +0900

    Tests for delegater

--- aws-xray-recorder-sdk-core/src/test/java/com/amazonaws/xray/emitters/DelegatingEmitterTest.java
+++ aws-xray-recorder-sdk-core/src/test/java/com/amazonaws/xray/emitters/DelegatingEmitterTest.java
@@ -1,8 +1,13 @@
 package com.amazonaws.xray.emitters;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import com.amazonaws.xray.entities.Segment;
-import com.amazonaws.xray.entities.SegmentImpl;
 import com.amazonaws.xray.entities.Subsegment;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -23,11 +28,21 @@ public class DelegatingEmitterTest {
     @Mock
     private Subsegment subsegment;
 
+    @Before
+    public void setUp() {
+        when(emitter.sendSegment(any())).thenReturn(true);
+        when(emitter.sendSubsegment(any())).thenReturn(true);
+    }
+
     @Test
     public void delegates() {
         Emitter delegator = new DelegatingEmitter(emitter);
 
-        assertThat(delegator.sendSegment(segment));
+        assertThat(delegator.sendSegment(segment)).isTrue();
+        verify(emitter).sendSegment(segment);
+
+        assertThat(delegator.sendSubsegment(subsegment)).isTrue();
+        verify(emitter).sendSubsegment(subsegment);
 
     }
 }
