ffd1463b47b101246899a8c8b1599a3506c244c5
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-06-11 16:30:36 +0900

    Depend on aws sdk modules from instrumentor for runtime rather than compile only.

--- aws-xray-recorder-sdk-aws-sdk-instrumentor/build.gradle.kts
+++ aws-xray-recorder-sdk-aws-sdk-instrumentor/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    compileOnly(project(":aws-xray-recorder-sdk-aws-sdk"))
+    implementation(project(":aws-xray-recorder-sdk-aws-sdk"))
 }
 
 description = "AWS X-Ray Recorder SDK for Java - AWS SDK Instrumentor"
--- aws-xray-recorder-sdk-aws-sdk-v2-instrumentor/build.gradle.kts
+++ aws-xray-recorder-sdk-aws-sdk-v2-instrumentor/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 dependencies {
-    compileOnly(project(":aws-xray-recorder-sdk-aws-sdk-v2"))
+    implementation(project(":aws-xray-recorder-sdk-aws-sdk-v2"))
 }
 
 description = "AWS X-Ray Recorder SDK for Java - AWS SDK V2 Instrumentor"
