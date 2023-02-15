658fe6170d9f404e9000d7c6c8bf7cef0b9d27ef
Adam Shaw <atshaw@amazon.com>
2023-02-15 09:58:58 -0800

    Changing jakarta version

--- aws-xray-recorder-sdk-core/build.gradle.kts
+++ aws-xray-recorder-sdk-core/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
 
     compileOnly("com.google.code.findbugs:jsr305:3.0.2")
     compileOnly("javax.servlet:javax.servlet-api:3.1.0")
-    compileOnly("jakarta.servlet:jakarta.servlet-api:4.0.2")
+    compileOnly("jakarta.servlet:jakarta.servlet-api:5.0.0")
 
     testImplementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     testImplementation("com.github.stefanbirkner:system-rules:1.16.0")
@@ -17,7 +17,7 @@ dependencies {
     testImplementation("org.powermock:powermock-module-junit4:2.0.7")
     testImplementation("org.powermock:powermock-api-mockito2:2.0.7")
     testImplementation("org.skyscreamer:jsonassert:1.3.0")
-    testImplementation("jakarta.servlet:jakarta.servlet-api:4.0.2")
+    testImplementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
 }
 
 tasks.jar {
