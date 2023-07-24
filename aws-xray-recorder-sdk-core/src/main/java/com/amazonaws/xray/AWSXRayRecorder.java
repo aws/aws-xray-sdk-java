34bfffe9c43c4806ee0d77be8a3703694dd56b28
atshaw43 <108552302+atshaw43@users.noreply.github.com>
2023-07-24 14:39:44 -0700

    Update AWSXRayRecorder.java

--- aws-xray-recorder-sdk-core/src/main/java/com/amazonaws/xray/AWSXRayRecorder.java
+++ aws-xray-recorder-sdk-core/src/main/java/com/amazonaws/xray/AWSXRayRecorder.java
@@ -167,9 +167,9 @@ public class AWSXRayRecorder {
         LambdaSegmentContextResolver lambdaSegmentContextResolver = new LambdaSegmentContextResolver();
         if (lambdaSegmentContextResolver.resolve() != null) {
             segmentContextResolverChain.addResolver(lambdaSegmentContextResolver);
-        } else {
-            segmentContextResolverChain.addResolver(new ThreadLocalSegmentContextResolver());
         }
+        
+        segmentContextResolverChain.addResolver(new ThreadLocalSegmentContextResolver());
 
         segmentListeners = new ArrayList<>();
 
