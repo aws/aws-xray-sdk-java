c27503593cafe438dbf94d91442b33e801e537b1
Mahad Janjua <134644284+majanjua-amzn@users.noreply.github.com>
2024-05-06 16:17:35 -0700

    [Lambda] Send NoOp segment when trace header is incomplete (#403)
    * [Lambda] Send NoOp segment when trace header is incomplete
    
    * [Lambda] Add debug log and root lambda passthrough test case
    
    * Fix checkstyle

--- aws-xray-recorder-sdk-core/src/main/java/com/amazonaws/xray/entities/NoOpSegment.java
+++ aws-xray-recorder-sdk-core/src/main/java/com/amazonaws/xray/entities/NoOpSegment.java
@@ -22,7 +22,7 @@ import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.locks.ReentrantLock;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-class NoOpSegment implements Segment {
+public class NoOpSegment implements Segment {
 
     private final TraceID traceId;
     private final AWSXRayRecorder creator;
